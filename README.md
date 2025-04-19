b5d950f40d111b7540489f910c16c0c5eadc0717
hemaxpatel <hemaxpatel3124@gmail.com>
2025-04-19 17:37:17 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# SABRAS Mukhwas Online Store/Ecommerce
+
+update env,
+mongodb change
+
+## Features
+
+- User Panel
+- Admin Panel
+- JWT Authentication
+- Password Hashing with Bcrypt
+- Stripe Payment Integration
+- Login/Signup
+- Logout
+- Add to Cart
+- Place Order
+- Order Management
+- Products Management
+- Filter Products
+- Login/Signup
+- Authenticated APIs
+- REST APIs
+- Role-Based Identification
+- Beautiful Alerts
+
+## Tech Stack
+
+- [React](https://reactjs.org/)
+- [Node.js](https://nodejs.org/en)
+- [Express.js](https://expressjs.com/)
+- [Mongodb](https://www.mongodb.com/)
+- [Stripe](https://stripe.com/)
+- [JWT-Authentication](https://jwt.io/introduction)
+- [Multer](https://www.npmjs.com/package/multer)
---
+++ frontend/src/assets/frontend_assets/assets.js
@@ -0,0 +1,68 @@
+import basket_icon from './basket_icon.png'
+import logo from './new/logo.jpg'
+import header_img from './header.png'
+import search_icon from './search_icon.png'
+import menu_1 from './new/mukhwas1.jpg'
+import menu_2 from './new/paan1.jpg'
+import menu_3 from './new/gulkand1.jpg'
+import menu_4 from './new/candy1.jpg'
+import menu_5 from './new/mewa1.jpg'
+
+
+import add_icon_white from './add_icon_white.png'
+import add_icon_green from './add_icon_green.png'
+import remove_icon_red from './remove_icon_red.png'
+import linkedin_icon from './linkedin_icon.png'
+import facebook_icon from './facebook_icon.png'
+import twitter_icon from './twitter_icon.png'
+import cross_icon from './cross_icon.png'
+import selector_icon from './selector_icon.png'
+import rating_starts from './rating_starts.png'
+import profile_icon from './profile_icon.png'
+import bag_icon from './bag_icon.png'
+import logout_icon from './logout_icon.png'
+import parcel_icon from './parcel_icon.png'
+
+export const assets = {
+    logo,
+    basket_icon,
+    header_img,
+    search_icon,
+    rating_starts,
+    add_icon_green,
+    add_icon_white,
+    remove_icon_red,
+    linkedin_icon,
+    facebook_icon,
+    twitter_icon,
+    cross_icon,
+    selector_icon,
+    profile_icon,
+    logout_icon,
+    bag_icon,
+    parcel_icon
+}
+
+export const menu_list = [
+    {
+        menu_name: "Mukhwas",
+        menu_image: menu_1
+    },
+    {
+        menu_name: "Paan",
+        menu_image: menu_2
+    },
+    {
+        menu_name: "Gulkand",
+        menu_image: menu_3
+    },
+    {
+        menu_name: "Candy",
+        menu_image: menu_4
+    },
+    {
+        menu_name: "Mewa",
+        menu_image: menu_5
+    }
+]
+    
